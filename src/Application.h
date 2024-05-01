8a95a972a8d0b90536c96871753a85a3fb7e9c8f
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-05-01 20:10:56 +0800

    Impl app

--- src/Application.h
+++ src/Application.h
@@ -2,8 +2,63 @@
 
 #include <PCH.h>
 
-#include "Configuration.h"
 #include "Util/Singleton.h"
+
+enum class MFMEntryType : std::uint32_t
+{
+    kRegular = 0,
+    kDirectory = 1,
+};
+
+class MFMEntry
+{
+public:
+    MFMEntry(std::filesystem::path a_path, MFMEntryType a_type) :
+        path(std::move(a_path)), name(PathToStr(path)), type(a_type)
+    {
+        BuildChild();
+    }
+
+    friend bool operator==(const MFMEntry& lhs, const MFMEntry& rhs) noexcept { return lhs.path == rhs.path; }
+
+    friend std::strong_ordering operator<=>(const MFMEntry& lhs, const MFMEntry& rhs) noexcept
+    {
+        return lhs.path <=> rhs.path;
+    }
+
+private:
+    void BuildChild();
+
+public:
+    std::filesystem::path path;
+    std::string           name;
+    MFMEntryType          type;
+    std::vector<MFMEntry> children;
+};
+
+class MFMRoot
+{
+public:
+    explicit MFMRoot(std::filesystem::path a_root) : root(std::move(a_root), MFMEntryType::kDirectory)
+    {
+        CurrentRoot(root);
+    }
+
+    MFMEntry* CurrentRoot() { return currentRoot; }
+    void      CurrentRoot(MFMEntry& a_root) { CurrentRoot(std::addressof(a_root)); }
+    void      CurrentRoot(MFMEntry* a_root)
+    {
+        currentRoot = a_root;
+        currentRootStr = PathToStr(a_root->path).substr(39);
+    }
+
+    const std::string& CurrentRootStr() const { return currentRootStr; }
+
+private:
+    MFMEntry    root;
+    MFMEntry*   currentRoot;
+    std::string currentRootStr;
+};
 
 class Application final : public Singleton<Application>
 {
@@ -12,12 +67,12 @@ class Application final : public Singleton<Application>
 public:
     //static bool IsMenu(std::string_view a_menuName);
 
-    void ToggleUI();
-
 private:
     Application() = default;
 
-    //static bool IsInMenu(const Configuration* a_config, RE::UI* a_ui);
-    //static bool IsInBannedMenu(const Configuration* a_config, RE::UI* a_ui);
-    //static bool IsInMenuContext(const Configuration* a_config, RE::UI* a_ui);
+public:
+    MFMRoot modRoot{ L"Data/SKSE/Plugins/ccld_ModFunctionMenu/Mod/"sv };
+    MFMRoot configRoot{ L"Data/SKSE/Plugins/ccld_ModFunctionMenu/Config/"sv };
+
+    static inline const std::filesystem::path root{ L"Data/SKSE/Plugins/ccld_ModFunctionMenu/"sv };
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,6 +5,7 @@
 #include <imgui_impl_win32.h>
 #include <imgui_internal.h>
 
+#include "Application.h"
 #include "ImGui/Renderer.h"
 #include "InputManager.h"
 
@@ -31,4 +32,19 @@ void Menu::Toggle()
     }
 }
 
-void Menu::Draw() { ImGui::ShowDemoWindow(); }
+void Menu::Draw()
+{
+    auto app = Application::GetSingleton();
+
+    auto viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->GetCenter(), ImGuiCond_Always, ImVec2{ 0.5f, 0.5f });
+    ImGui::SetNextWindowSize(ImVec2{ viewport->Size.x * 0.5f, viewport->Size.y * 0.5f });
+
+    ImGui::Begin("Mod Function Menu");
+    {
+        auto& root = app->modRoot;
+        ImGui::Text("%s", root.CurrentRootStr().c_str());
+    }
+    ImGui::End();
+    //ImGui::ShowDemoWindow();
+}
