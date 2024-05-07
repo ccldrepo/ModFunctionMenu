8848dcd0ec5e9593400a22feca2dce35a73bae66
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-05-07 22:30:10 +0800

    Add _ prefix to private data member

--- src/InputManager.h
+++ src/InputManager.h
@@ -12,26 +12,26 @@ public:
         kWantUnblock = 2,
     };
 
-    static bool IsNotBlocked() noexcept { return state.load() == BlockState::kNotBlocked; }
+    static bool IsNotBlocked() noexcept { return _state.load() == BlockState::kNotBlocked; }
 
-    static void SetBlocked() noexcept { state.store(BlockState::kBlocked); }
+    static void SetBlocked() noexcept { _state.store(BlockState::kBlocked); }
 
     static void SetWantUnblock() noexcept
     {
         auto expected = BlockState::kBlocked;
-        state.compare_exchange_strong(expected, BlockState::kWantUnblock);
+        _state.compare_exchange_strong(expected, BlockState::kWantUnblock);
     }
 
     static void HandleWantUnblock() noexcept
     {
-        auto currentState = state.load();
+        auto currentState = _state.load();
         if (currentState == BlockState::kWantUnblock) {
-            state.compare_exchange_strong(currentState, BlockState::kNotBlocked);
+            _state.compare_exchange_strong(currentState, BlockState::kNotBlocked);
         }
     }
 
     static void Process(RE::InputEvent* const* a_event);
 
 private:
-    static inline std::atomic<BlockState> state{ BlockState ::kNotBlocked };
+    static inline std::atomic<BlockState> _state{ BlockState ::kNotBlocked };
 };
