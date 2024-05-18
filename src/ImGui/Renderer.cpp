704b9efcfdf1c3d11ddee93e94fc60d837dd1580
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-05-18 17:38:08 +0800

    Add include dir

--- src/ImGui/Renderer.cpp
+++ src/ImGui/Renderer.cpp
@@ -2,9 +2,9 @@
 
 #include <dxgi.h>
 
-#include <backends/imgui_impl_dx11.h>
-#include <backends/imgui_impl_win32.h>
 #include <imgui.h>
+#include <imgui_impl_dx11.h>
+#include <imgui_impl_win32.h>
 #include <imgui_internal.h>
 
 #include "../CLib/Hook.h"
